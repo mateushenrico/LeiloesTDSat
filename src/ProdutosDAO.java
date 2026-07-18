a79d8b77f9b936246df89aeb50578ba423f4a92b
Mateus Oliveira <mateushenrico@gmail.com>
2026-07-18 15:35:18 -0300

    Implementada listagem de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -44,8 +44,41 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+        listagem.clear();
+
+        String sql = "SELECT * FROM produtos";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+
+            prep = conn.prepareStatement(sql);
+
+            resultset = prep.executeQuery();
+
+            while(resultset.next()){
+
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+
+            }
+
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null,
+                    "Erro ao listar produtos.\n" + e.getMessage());
+
+        }
+
         return listagem;
+
     }
     
     
